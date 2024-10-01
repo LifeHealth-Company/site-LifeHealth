1f6591e339f2a376fd3c7c291f91a49697fa5746
raygrs <rayane.greis@sptech.school>
2024-10-01 01:16:50 -0300

    Arrumando flexibilidade do conteúdo principal e adicionando dark mode ainda não finalizado

--- public/menu/main.js
+++ public/menu/main.js
@@ -3,3 +3,13 @@ let btn = document.querySelector('#btn');
   btn.onclick = function () {
     sidebar.classList.toggle('active');
   };
+
+  let trilho = document.getElementById('trilho');
+  let body = document.querySelector('body');
+  trilho.addEventListener('click', () =>{
+    trilho.classList.toggle('dark')
+    body.classList.toggle('dark')
+
+  })
+
+  
